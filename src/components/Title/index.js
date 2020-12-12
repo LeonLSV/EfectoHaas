b9907c268677de3d7aebeec00c2a2dd191cb8413
Leon Ramiro <racramiro@gmail.com>
2020-12-12 14:41:08 -0500

    Title

---
+++ src/components/Title/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Row, Col } from "antd";
+
+export const Title = () => {
+  return (
+    <Row>
+      <Col span={24}>
+        <h1>Efecto Haas</h1>
+      </Col>
+    </Row>
+  );
+};
