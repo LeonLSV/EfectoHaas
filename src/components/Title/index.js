d9991c886fb1bf63dab0fb195b3b5e52aa3e4732
Leon Ramiro <racramiro@gmail.com>
2020-12-12 15:45:43 -0500

    Home

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { Row, Col } from "antd";
+import logonegro from "../../images/logonegro.png";
+import { TitleHome, LogoHome } from "./style";
+
+const centerTodo = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  margin: "0",
+};
 
 export const Title = () => {
   return (
-    <Row>
-      <Col span={24}>
-        <h1>Efecto Haas</h1>
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col span={24} style={centerTodo}>
+          <TitleHome margintop={"150px"}>Efecto</TitleHome>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24} style={centerTodo}>
+          <TitleHome margintop={"-50px"}>Haas</TitleHome>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24} style={centerTodo}>
+          <LogoHome src={logonegro} alt="Logo LSV" />
+        </Col>
+      </Row>
+    </>
   );
 };
---
+++ src/components/Title/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const TitleHome = styled.h1`
+  font-size: 120px;
+  margin: 0;
+  color: white;
+  text-shadow: black 2px 5px;
+  margin-top: ${(props) => props.margintop};
+  font-family: "Ranchers", cursive;
+`;
+
+export const LogoHome = styled.img`
+  width: 300px;
+  margin: 0;
+  margin-top: 120px;
+`;
